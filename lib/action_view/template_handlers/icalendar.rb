2e7ce809f1e2417e7ff41bd419af2cc3a4af9417
Matthew Todd <matthew.todd@gmail.com>
2008-11-18 15:49:02 +0300

    Update for Rails 2.2.

--- lib/action_view/template_handlers/icalendar.rb
+++ lib/action_view/template_handlers/icalendar.rb
@@ -3,16 +3,11 @@ module ActionView
     class Icalendar < TemplateHandler
       include Compilable
 
-      def self.line_offset
-        2
-      end
-
       def compile(template)
-        content_type_handler = (@view.send!(:controller).respond_to?(:response) ? "controller.response" : "controller")
-        "#{content_type_handler}.content_type ||= Mime::ICS\n" +
-        "cal ||= ::Icalendar::Calendar.new\n" +
+        "_set_controller_content_type(Mime::ICS);" +
+        "cal ||= ::Icalendar::Calendar.new;" +
         template.source +
-        "\ncal.to_ical\n"
+        ";cal.to_ical;"
       end
     end
   end
